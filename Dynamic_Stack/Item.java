83328bd060a3bcfd7595863cab6e5ffa8ff42c1e
aljo <johansson_a95@hotmail.com>
2023-09-02 23:33:10 +0200

    preparation files

---
+++ Dynamic_Stack/Item.java
@@ -0,0 +1,55 @@
+
+package com.mycompany.hp35dynamic;
+
+public class Item {
+
+    ItemType type;
+    int value = 0;
+
+    ItemType type() {
+        return this.type;
+    }
+    
+    public enum ItemType {
+        ADD,
+        SUB,
+        MUL,
+        DIV,
+        VALUE
+    }
+    
+    static Item Value(int i) {
+        Item tempItem = new Item();
+        tempItem.type = ItemType.VALUE;
+        tempItem.value = i;
+        return tempItem;
+    }
+    
+    static Item Add() {
+        Item tempItem = new Item();
+        tempItem.type = ItemType.ADD;
+        tempItem.value = 0;
+        return tempItem;
+    }
+    
+    static Item Mul() {
+        Item tempItem = new Item();
+        tempItem.type = ItemType.MUL;
+        tempItem.value = 0;
+        return tempItem;
+    }
+    
+     static Item Sub() {
+        Item tempItem = new Item();
+        tempItem.type = ItemType.SUB;
+        tempItem.value = 0;
+        return tempItem;
+    }
+        
+    static Item Div() {
+        Item tempItem = new Item();
+        tempItem.type = ItemType.DIV;
+        tempItem.value = 0;
+        return tempItem;
+    }
+}
