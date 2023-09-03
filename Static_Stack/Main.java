fe04cf010049f5de43b36a4a55607ae345e4fdfe
aljo <johansson_a95@hotmail.com>
2023-09-03 23:21:46 +0200

    first files in order to create stack

---
+++ Static_Stack/Main.java
@@ -0,0 +1,23 @@
+
+package com.mycompany.hp35stack;
+
+/**
+ * @author Alexander Johansson
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Item[] expr = {
+            Item.Value(10),
+            Item.Value(2),
+            Item.Value(5),
+            Item.Mul(),
+            Item.Add()
+        };
+        
+        Calculator calc = new Calculator(expr);
+        int res = calc.run();
+        System.out.println(" Calculator: res = " + res); 
+    }
+}
